c9c6481a62babeec7d802540480f0dd36fec4a55
Dillion Megida <dillionmegida@gmail.com>
2023-06-04 22:58:17 +0200

    init with gatsby

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+
+const IndexPage = () => (
+  <h1>Simple Regex</h1>
+)
+
+export default IndexPage
