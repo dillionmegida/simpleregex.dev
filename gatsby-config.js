7bb8319bbd16a40af7f5c1b3b3f0a56e2668231e
Dillion Megida <dillionmegida@gmail.com>
2023-06-05 17:14:55 +0200

    feat: add google tag

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,4 @@
 /**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
- */
-
-/**
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
@@ -32,12 +26,25 @@ module.exports = {
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+        resolve: `gatsby-plugin-google-gtag`,
+        options: {
+          trackingIds: [
+            "G-YNPDWE075X",
+          ],
+          gtagConfig: {
+            anonymize_ip: true,
+            cookie_expires: 0,
+          },
+          pluginConfig: {
+            head: false,
+            delayOnRouteUpdate: 0,
+          },
+        },
+      },
   ],
 }
