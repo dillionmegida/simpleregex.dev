d287f972bedbefbd432b7df6ab64d478b86ba4dd
Dillion Megida <dillionmegida@gmail.com>
2023-06-06 10:06:37 +0200

    fix some bugs in build

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,28 +1,14 @@
 import * as React from "react"
 import { Link } from "gatsby"
 
-const Layout = ({ title }) => {
-//   const rootPath = `${__PATH_PREFIX__}/`
-//   const isRootPath = location.pathname === rootPath
-//   let header
-
-//   if (isRootPath) {
-//     header = (
-//       <h1 className="main-heading">
-//         <Link to="/">{title}</Link>
-//       </h1>
-//     )
-//   } else {
-    header = (
-      <Link className="header-link-home" to="/">
-        {title}
-      </Link>
-    )
-//   }
-
+const Layout = ({ title, children }) => {
   return (
-    <div className="global-wrapper" data-is-root-path={isRootPath}>
-      <header className="global-header">{header}</header>
+    <div className="global-wrapper">
+      <header className="global-header">
+        <Link className="header-link-home" to="/">
+          {title}
+        </Link>
+      </header>
       <main>{children}</main>
       <footer>© {new Date().getFullYear()}</footer>
     </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ const IndexPage = ({ data, location }) => {
 
   if (posts.length === 0) {
     return (
-      <Layout location={location} title={siteTitle}>
+      <Layout title={siteTitle}>
         <Bio />
         <p>
           No blog posts found. Add markdown posts to "content/blog" (or the
@@ -23,7 +23,7 @@ const IndexPage = ({ data, location }) => {
   }
 
   return (
-    <Layout location={location} title={siteTitle}>
+    <Layout title={siteTitle}>
       <Bio />
       <ol style={{ listStyle: `none` }}>
         {/* {posts.map(post => {
