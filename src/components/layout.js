cfe049c54ebac0e14023e7f4c4c722c2be74c2dd
Dillion Megida <dillionmegida@gmail.com>
2023-06-06 10:17:14 +0200

    feat: add course cover

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,13 +1,28 @@
 import * as React from "react"
 import { Link } from "gatsby"
+import styled from "styled-components"
+
+const Cover = styled.div`
+  with: 100%;
+
+  img {
+    width: 100%;
+  }
+`
 
 const Layout = ({ title, children }) => {
   return (
     <div className="global-wrapper">
       <header className="global-header">
         <Link className="header-link-home" to="/">
-          {title}
+          <h1>{title}</h1>
         </Link>
+        <Cover className="cover">
+          <img
+            src="/regex-course-cover.png"
+            alt="Regular Expressions Course Cover"
+          />
+        </Cover>
       </header>
       <main>{children}</main>
       <footer>© {new Date().getFullYear()}</footer>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
 import * as React from "react"
-import { Link, graphql } from "gatsby"
+import { graphql } from "gatsby"
 
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const IndexPage = ({ data, location }) => {
+const IndexPage = ({ data }) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`
   const posts = data.allMarkdownRemark.nodes
 
